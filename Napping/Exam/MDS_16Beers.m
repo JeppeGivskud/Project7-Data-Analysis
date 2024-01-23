a4b9817a527aec3ce1bcdd9fd82eb65af7748a3c
JeppeGivskud <jgivsk20@student.aau.dk>
2024-01-23 10:51:32 +0100

    Corrections
    I've added a correct version of the 16 Beers MDS scaling. Matlab can aparently estimate missing values quite fine by itself using a "start" "Random" argument to the mdscale function

--- Napping/Exam/MDS_16Beers.m
+++ Napping/Exam/MDS_16Beers.m
@@ -2,17 +2,21 @@
 % But its just some basic multidimensional scaling functions and som plot parameters
 clear
 clc
+T = readcell('16 Beers.xlsx','UseExcel',true,'Sheet','For Matlab');
+Beernames=T(2:end,1);
+Beernames=string(Beernames);
 
-T = readtable("16Beers.xlsx");
-Beernames=table2array(T(1:end,1));
+T=T(2:end,2:end);
+T = cell2table(T);
+T = table2array(T);
 
-T=T(1:end,2:end)
-T = table2array(T)
 
 %% Create a scree plot
 A = [0 0 0 0 0 0];
 for c = 1:6
-    [Y1,stress1,eigvals1] = mdscale(T,c);
+    [Y,stress1] = mdscale(T,c,'Start','random');
+
+    %[Y1,stress1,eigvals1] = mdscale(T,p);
     A(c)=stress1;
 end
 fprintf('Stress for Test1: %.4f\n',A);
@@ -22,15 +26,15 @@ hold on
 plot(A,'DisplayName','Collected Data')
 xlabel('Dimensions')
 ylabel('Stress')
-title('Scree Plot')
+title('Scree Plot 16 beers NaN values')
 set(gca, 'xtick', 0:1:10);
-set(gca, 'ytick', 0:0.01:10);
+set(gca, 'ytick', 0:0.02:10);
 lgd = legend;
 hold off
 
 %% - Multidimensional scaling plot
 
-[Y1,stress1,eigvals1] = mdscale(T,3);
+[Y1,stress1] = mdscale(T,3,'Start','random')
 figure;
 plot3(Y1(:,1),Y1(:,2),Y1(:,3),'o');
 hold on
@@ -39,9 +43,12 @@ for i=1:length(Y1)
     bottom = [Y1(i,1) Y1(i,2) -15];
     together = [top ; bottom];
     plot3(together(:,1),together(:,2),together(:,3),'--oblack')
+    text(Y1(i,1), Y1(i,2), Y1(i,3),Beernames(i))
+
 end
 title("Multidimensional scaling of napping results");
-text(Y1(:,1)+1,Y1(:,2),Y1(:,3),Beernames)
+%text(Y1(:,1)+1,Y1(:,2),Y1(:,3),Beernames)
+
 xlabel('Fruityness (Frugtighed) -->') %Dim1
 ylabel('<-- Power (Kraftighed)') %Dim2
 zlabel('Color (Farve)-->') %Dim3
@@ -55,6 +62,7 @@ for ii = 1:5000
     drawnow;
     view(gca,[ii, 35]);
 
+    pause(0.2)
     
 end
 hold off
